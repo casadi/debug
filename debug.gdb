1345bb7ff7b7d710fbee3ecc661971b9e18c8d4a
Joris Gillis <joris.gillis42@gmail.com>
2026-01-29 15:49:47 +0100

    debug

---
+++ debug.gdb
@@ -0,0 +1,9 @@
+set pagination off
+set breakpoint pending on
+break IntegerSX
+commands
+  bt
+  continue
+end
+run
+quit
--- debug.py
+++ debug.py
@@ -2,11 +2,9 @@ from casadi import *
 
 x = SX.sym('x', Sparsity.diag(4))
 
-print(SX(-2))
-
 print(SX(1,1)**(-2))
 
-x = MX.sym('x', Sparsity.diag(4))
+#x = MX.sym('x', Sparsity.diag(4))
 
-print(MX(-2))
-print(x**(-2))
+#print(MX(-2))
+#print(x**(-2))
