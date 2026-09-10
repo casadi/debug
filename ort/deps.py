394e7ef458a0ad7732941cd7b5bca2fe52c975d6
Joris Gillis <joris@yacoda.com>
2026-09-10 10:40:26 +0200

    casadi: redo the PATH dependency test natively (MSYS mangles windows PATH entries)

--- ort/deps.py
+++ ort/deps.py
@@ -7,6 +7,7 @@ casdir = os.path.dirname(ca.__file__)
 depdir = os.path.join(os.path.dirname(casdir), "moved_deps")
 print("mode:", mode)
 print("CASADI_PLUGIN_SEARCH_PATH:", os.environ.get("CASADI_PLUGIN_SEARCH_PATH"))
+print("depdir on PATH:", any(os.path.normcase(x.rstrip("\\")) == os.path.normcase(depdir) for x in os.environ.get("PATH","").split(os.pathsep)))
 print("libhighs in casadi dir:", os.path.exists(os.path.join(casdir, "libhighs.dll")))
 print("libhighs in moved dir :", os.path.exists(os.path.join(depdir, "libhighs.dll")))
 try:
